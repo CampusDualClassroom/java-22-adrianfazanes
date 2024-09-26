410340712ce18a7a39db2364e160906601cdf32c
Adrián Fazanes Porto <adrifazanes38@gmail.com>
2024-09-26 10:31:36 +0200

    Ejercicio 22 Resuelto

--- src/main/java/com/campusdual/classroom/Exercise22.java
+++ src/main/java/com/campusdual/classroom/Exercise22.java
@@ -2,25 +2,49 @@ package com.campusdual.classroom;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 
 public class Exercise22 {
 
-    public static List<String> createArrayList() {
 
+    public static List<String> createArrayList() {
+        List<String> customList = new ArrayList<>();
+
+        customList.add("GKFFD");
+        customList.add("TNANA");
+        customList.add("MPMSL");
+        customList.add("PSWME");
+        customList.add("LZMLF");
+        customList.add("JYEBV");
+        customList.add("YELNT");
+        customList.add("JSNKR");
+        customList.add("JFESF");
+        customList.add("TMJLL");
+
+
+        return customList;
     }
+
 
     public static void printElementsAndIndex(List<String> customList) {
 
+        for (int i = 0; i < customList.size(); i++) {
+            System.out.println("Índice: " + i + ", Elemento: " + customList.get(i));
+        }
     }
 
-    public static boolean addElementToList(List<String> customList, String element) {
 
+    public static boolean addElementToList(List<String> customList, String element) {
+        return customList.add(element);
     }
 
     public static void main(String[] args) {
 
+        List<String> customList = createArrayList();
+
+
+        boolean isAdded = addElementToList(customList, "AAAAA");
+        System.out.println("¿Elemento añadido exitosamente? " + isAdded);
+
+        printElementsAndIndex(customList);
     }
-
-
 }
